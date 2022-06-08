c24b19c591481c68da49bf7c70ff8523466f056a
Rajveer Singh <rajveersingh030101@gmail.com>
2022-06-08 16:04:37 +0800

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize("grabportallogin", "root", "",
+module.exports = new Sequelize("grabportallogin", "root", "root",
  {
   host: "localhost",
   dialect: "mysql",

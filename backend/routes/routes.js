bb6ca27956f9e22b9f39f094b582dc9f29dd33b2
Jasmine-18 <jasmine_hweeying@hotmail.com>
2022-06-08 15:17:13 +0800

    Use sequelize(ORM)

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,50 +1,45 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
-const mysql = require("mysql");
+// const mysql = require("mysql");
+// const UserModel = require("../models/user");
+const db = require("../config/database");
 const jwt = require("jsonwebtoken");
 
 // Connect to user account MySQL database
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "grabportallogin",
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "grabportallogin",
+// });
 
 // http://localhost:8080/api/login
 router.post("/login", function (req, res) {
   // Capture the input fields
   let username = req.body.username;
   let password = req.body.password;
-  let userQuery =
-    "SELECT * FROM accounts WHERE username = " + connection.escape(username);
   // Ensure the input fields exists and are not empty
   if (username && password) {
     // Retrive user data
-    connection.query(userQuery, function (error, results, fields) {
-      if (error) throw error;
-      // Ensure the username is exist and correct password
-      if (results.length == 0) {
-        res.status(404).send({
-          message: "User not found",
-        });
-      } else if (results.length > 0 && password === results[0].password) {
-        // Signed using secret key
-        const token = jwt.sign({ id: results[0].username }, "secret");
-        // Store token as cookie
-        res.cookie("jwt", token, {
-          httpOnly: true,
-          maxAge: 15 * 1000,
-        });
-        res.send({
-          message: "success",
-        });
-      } else {
-        res.status(403).send({
-          message: "Incorrect password",
-        });
-      }
-    });
+    return db
+      .query("SELECT * FROM accounts WHERE username = '" + username + "'")
+      .then((result) => {
+        // No result is found
+        if (result[0].length === 0) {
+          return res.status(404).send({ message: "User Not Found" });
+        } else if (result.length > 0 && result[0][0].password === password) {
+          // Signed using secret key
+          const token = jwt.sign({ id: result[0][0].username }, "secret");
+          // Store token as cookie
+          res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: 15 * 1000,
+          });
+          res.send({ message: "success" });
+        } else {
+          res.status(403).send({ message: "Incorrect password" });
+        }
+      });
   } else {
     res.send({
       message: "Please enter Username and Password!",
@@ -61,12 +56,13 @@ router.get("/auth", (req, res) => {
     return res.status(401).send({
       message: "unauthenticated",
     });
+  } else {
+    return db
+      .query("SELECT * FROM accounts WHERE username = '" + claims.id + "'")
+      .then((result) => {
+        res.send(result[0][0]);
+      });
   }
-  let userQuery =
-    "SELECT * FROM accounts WHERE username = " + connection.escape(claims.id);
-  connection.query(userQuery, function (error, results, fields) {
-    res.send(results);
-  });
 });
 
 // http://localhost:8080/logout

300be84dc88b56c162a7f7b8b91571e2411018f7
Jasmine-18 <jasmine_hweeying@hotmail.com>
2022-06-09 10:10:59 +0800

    solve sequelize query duplicate result

--- frontend/src/services/DataService.js
+++ frontend/src/services/DataService.js
@@ -1,7 +1,7 @@
 import http from "../http-common";
 class DataService {
   login(user) {
-    return http.post("/login",user);
+    return http.post("/login", user);
   }
   logout() {
     return http.post("/logout");

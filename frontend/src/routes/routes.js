a57ce3d049b7844ec7ac4b47e28ab401fd7ec912
Jasmine-18 <jasmine_hweeying@hotmail.com>
2022-06-13 16:40:26 +0800

    2.0

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -20,25 +20,7 @@ const routes = [
         name: "History",
         component: History
     }
-
-import { createWebHistory, createRouter } from "vue-router";
-import Login from "../components/Login.vue";
-import Dashboard from "../components/Dashboard.vue";
-
-const routes = [
-  {
-    path: "/",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: Dashboard,
-  },
-
-];
-
+  ];
 const router = createRouter({
   history: createWebHistory(),
   routes,
